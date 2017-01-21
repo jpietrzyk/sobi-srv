d38f50588ed00723d5c366952bf10262841a4e99
Jacek Pietrzyk <jacpie3k@gmail.com>
2017-01-21 10:00:16 +0100

    Update token generator to use SecureRandom

--- spec/support/spec_helpers/client_helper.rb
+++ spec/support/spec_helpers/client_helper.rb
@@ -1,6 +1,6 @@
 module ClientHelper extend self
                     def generate_token(length = 690)
-                      range = [*'0'..'9', *'A'..'Z', *'a'..'z']
-                      Array.new(length) { range.sample }.join
+                      require 'securerandom'
+                      SecureRandom.hex(345)
                     end
 end
